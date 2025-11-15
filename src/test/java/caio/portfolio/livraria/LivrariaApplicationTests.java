0900c1a2b16daa5c7580f1c579718e7aace13914
Caio <caio.vr12@gmail.com>
2025-11-15 10:31:54 -0300

    sobe projeto Spring Boot com dependência spring-boot-starter-web para criação de serviço RESTful

---
+++ src/test/java/caio/portfolio/livraria/LivrariaApplicationTests.java
@@ -0,0 +1,13 @@
+package caio.portfolio.livraria;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LivrariaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

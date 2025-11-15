87534734919831e32ecef0233df1b727ad413a1c
Caio <caio.vr12@gmail.com>
2025-11-15 20:35:27 -0300

    cria CountryController com endpoint para retorno de todos os países

---
+++ src/main/java/caio/portfolio/livraria/controller/country/CountryController.java
@@ -0,0 +1,25 @@
+package caio.portfolio.livraria.controller.country;
+
+import java.util.List;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import caio.portfolio.livraria.infrastructure.entity.Country;
+import caio.portfolio.livraria.service.CountryService;
+import lombok.RequiredArgsConstructor;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/countries")
+public class CountryController {
+
+	private final CountryService service;
+	
+	@GetMapping
+	public ResponseEntity<List<Country>> findAllCountries(){
+		return ResponseEntity.ok(service.getAllCountries());
+	}
+}

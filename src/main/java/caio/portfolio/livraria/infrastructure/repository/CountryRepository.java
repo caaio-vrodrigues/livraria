cec895a7d6dc9ec2c1b472aa6ea178b92a4848a9
Caio <caio.vr12@gmail.com>
2025-11-15 19:43:06 -0300

    cria repositório jpa para entidade Country

---
+++ src/main/java/caio/portfolio/livraria/infrastructure/repository/CountryRepository.java
@@ -0,0 +1,11 @@
+package caio.portfolio.livraria.infrastructure.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import caio.portfolio.livraria.infrastructure.entity.Country;
+
+@Repository
+public interface CountryRepository extends JpaRepository<Country, Integer> {
+
+}

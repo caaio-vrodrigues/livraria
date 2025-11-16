43c9042919f17de15c5fd3c02e543e3bd121fb00
Caio <caio.vr12@gmail.com>
2025-11-16 20:05:45 -0300

    adiciona tratamento para MethodArgumentNotValidException

--- src/main/java/caio/portfolio/livraria/controller/exception/ExceptionHandlerController.java
+++ src/main/java/caio/portfolio/livraria/controller/exception/ExceptionHandlerController.java
@@ -1,11 +1,13 @@
 package caio.portfolio.livraria.controller.exception;
 
-import java.time.LocalDate;
+import java.time.LocalDateTime;
 import java.util.LinkedHashMap;
+import java.util.List;
 import java.util.Map;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
@@ -16,7 +18,7 @@ import jakarta.servlet.http.HttpServletRequest;
 public class ExceptionHandlerController {
 
 	private static final String TIME_STAMP = "timestamp";
-	private static final String STATUS = "satatus";
+	private static final String STATUS = "status";
 	private static final String ERROR = "error";
 	private static final String MESSAGE = "message";
 	private static final String PATH = "path";
@@ -29,7 +31,7 @@ public class ExceptionHandlerController {
 		HttpServletRequest httpRequest
 	){
 		Map<String, Object> body = new LinkedHashMap<>();
-		body.put(TIME_STAMP, LocalDate.now());
+		body.put(TIME_STAMP, LocalDateTime.now());
 		body.put(STATUS, HttpStatus.BAD_REQUEST.value());
 		body.put(ERROR, HttpStatus.BAD_REQUEST.getReasonPhrase());
 		body.put(MESSAGE, e.getMessage());
@@ -37,4 +39,25 @@ public class ExceptionHandlerController {
 		body.put(DETAILS, "Argumento inválido fornecido na requisição");
 		return new ResponseEntity<>(body, HttpStatus.BAD_REQUEST);
 	}
+	
+	@ExceptionHandler(MethodArgumentNotValidException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public ResponseEntity<Object> handleMethodArgumentNotValidException(
+        MethodArgumentNotValidException e,
+        HttpServletRequest httpRequest
+    ){
+        Map<String, Object> body = new LinkedHashMap<>();
+        List<String> errors = e.getBindingResult()
+            .getFieldErrors()
+            .stream()
+            .map(error -> error.getField() +": "+ error.getDefaultMessage())
+            .toList();
+        body.put(TIME_STAMP, LocalDateTime.now());
+        body.put(STATUS, HttpStatus.BAD_REQUEST.value());
+        body.put(ERROR, HttpStatus.BAD_REQUEST.getReasonPhrase());
+        body.put(MESSAGE, "Erro de validação nos campos da requisição");
+        body.put(PATH, httpRequest.getRequestURI());
+        body.put(DETAILS, errors);
+        return new ResponseEntity<>(body, HttpStatus.BAD_REQUEST);
+    }
 }

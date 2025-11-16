ca56c4223040d51b2bd7a718a4368fcfd2b02886
Caio <caio.vr12@gmail.com>
2025-11-16 09:00:12 -0300

    implementa processIsoAlpha2Code para validação e normalização de código ISO Alpha 2

--- src/main/java/caio/portfolio/livraria/service/country/CountryValidatorImpl.java
+++ src/main/java/caio/portfolio/livraria/service/country/CountryValidatorImpl.java
@@ -1,5 +1,8 @@
 package caio.portfolio.livraria.service.country;
 
+import java.util.Arrays;
+import java.util.Locale;
+
 import org.springframework.stereotype.Service;
 
 import caio.portfolio.livraria.infrastructure.entity.country.dto.CreateCountryDTO;
@@ -15,7 +18,15 @@ public class CountryValidatorImpl implements CountryValidator {
 
 	@Override
 	public String processIsoAlpha2Code(String isoAlpha2Code) {
-		// TODO
+		boolean isIsoAlpha2CodeEmptyOrBlank = isoAlpha2Code.trim().isEmpty() || 
+			isoAlpha2Code.trim().isBlank();
+		
+		if(isIsoAlpha2CodeEmptyOrBlank) throw new IllegalArgumentException("O campo 'isoAlpha2Code' não pode estar vazio");
+		
+		String normalizedIsoAlpha2Code = isoAlpha2Code.toUpperCase();
+		boolean validIsoAlpha2Code = Arrays.asList(Locale.getISOCountries()).contains(normalizedIsoAlpha2Code);
+		if(!validIsoAlpha2Code) throw new IllegalArgumentException("O código '"+isoAlpha2Code+"' não corresponde a um código válido.");
+		  
 		return null;
 	}
 

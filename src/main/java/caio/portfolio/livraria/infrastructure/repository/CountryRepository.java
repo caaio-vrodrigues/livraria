05293b9766e50daca0ac052607c750684d2c9495
Caio <caio.vr12@gmail.com>
2025-11-16 10:41:07 -0300

    declara método findByIsoAlpha2Code para busca de país pelo código iso

--- src/main/java/caio/portfolio/livraria/infrastructure/repository/CountryRepository.java
+++ src/main/java/caio/portfolio/livraria/infrastructure/repository/CountryRepository.java
@@ -1,5 +1,7 @@
 package caio.portfolio.livraria.infrastructure.repository;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -7,5 +9,5 @@ import caio.portfolio.livraria.infrastructure.entity.country.Country;
 
 @Repository
 public interface CountryRepository extends JpaRepository<Country, Integer> {
-
+	Optional<Country> findByIsoAlpha2Code(String isoAlpha2Code);
 }

8e84bf579ff24ba1510f03f8dac6dd59163c1fc4
Caio <caio.vr12@gmail.com>
2025-11-16 10:55:11 -0300

    implementa método createOrFindCountry para retornar país existente ou criar um novo

--- src/main/java/caio/portfolio/livraria/service/country/CountryService.java
+++ src/main/java/caio/portfolio/livraria/service/country/CountryService.java
@@ -1,6 +1,7 @@
 package caio.portfolio.livraria.service.country;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -9,6 +10,8 @@ import caio.portfolio.livraria.infrastructure.entity.country.Country;
 import caio.portfolio.livraria.infrastructure.entity.country.dto.CreateCountryDTO;
 import caio.portfolio.livraria.infrastructure.entity.country.dto.ResponseCountryDTO;
 import caio.portfolio.livraria.infrastructure.repository.CountryRepository;
+import caio.portfolio.livraria.service.country.model.CountryValidator;
+import caio.portfolio.livraria.service.country.model.ResponseCountryDTOCreator;
 import lombok.RequiredArgsConstructor;
 
 @Service
@@ -16,10 +19,22 @@ import lombok.RequiredArgsConstructor;
 public class CountryService {
 
 	private final CountryRepository repo;
+	private final CountryValidator countryValidator;
+	private final ResponseCountryDTOCreator responseCountryDTOCreator;
 	
-	public ResponseCountryDTO createCountry(CreateCountryDTO createCountryDTO) {
-		// to-do
-		return null;
+	@Transactional
+	public ResponseCountryDTO createOrFindCountry(CreateCountryDTO createCountryDTO) {
+		String validIsoAlpha2Code = countryValidator.processIsoAlpha2Code(createCountryDTO.getIsoAlpha2Code());
+		Optional<Country> countryOptional = repo.findByIsoAlpha2Code(validIsoAlpha2Code);
+		boolean countryAlreadyExists = countryOptional.isPresent();
+		if(countryAlreadyExists) return responseCountryDTOCreator.toResponseCountryDTO(countryOptional.get());
+		String validCountryName = countryValidator.getNameByValidatedAndNormalizedIsoAlpha2Code(validIsoAlpha2Code);
+		Country newCountry = Country.builder()
+			.isoAlpha2Code(validIsoAlpha2Code)
+			.name(validCountryName)
+			.build();
+		repo.saveAndFlush(newCountry);
+		return responseCountryDTOCreator.toResponseCountryDTO(newCountry);
 	}
 	
 	@Transactional(readOnly=true)

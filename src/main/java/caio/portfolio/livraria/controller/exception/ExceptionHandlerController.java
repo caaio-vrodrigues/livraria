499180be2dd58e3588b18aa4d5168c0dc124df35
Caio <caio.vr12@gmail.com>
2025-11-17 20:01:37 -0300

    adiciona tratamento para CountryNotFoundException

--- src/main/java/caio/portfolio/livraria/controller/exception/ExceptionHandlerController.java
+++ src/main/java/caio/portfolio/livraria/controller/exception/ExceptionHandlerController.java
@@ -12,6 +12,7 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
+import caio.portfolio.livraria.exception.custom.CountryNotFoundException;
 import jakarta.servlet.http.HttpServletRequest;
 
 @RestControllerAdvice
@@ -23,6 +24,22 @@ public class ExceptionHandlerController {
 	private static final String MESSAGE = "message";
 	private static final String PATH = "path";
 	private static final String DETAILS = "details";
+	
+	@ExceptionHandler(CountryNotFoundException.class)
+	@ResponseStatus(HttpStatus.NOT_FOUND)
+	public ResponseEntity<Object> handleCountryNotFoundException(
+	    CountryNotFoundException e,
+	    HttpServletRequest httpRequest
+	){
+	    Map<String, Object> body = new LinkedHashMap<>();
+	    body.put(TIME_STAMP, LocalDateTime.now());
+	    body.put(STATUS, HttpStatus.NOT_FOUND.value());
+	    body.put(ERROR, HttpStatus.NOT_FOUND.getReasonPhrase());
+	    body.put(MESSAGE, e.getMessage());
+	    body.put(PATH, httpRequest.getRequestURI());
+	    body.put(DETAILS, "O país solicitado não foi encontrado no sistema");
+	    return new ResponseEntity<>(body, HttpStatus.NOT_FOUND);
+	}
 	
 	@ExceptionHandler(IllegalArgumentException.class)
 	@ResponseStatus(HttpStatus.BAD_REQUEST)

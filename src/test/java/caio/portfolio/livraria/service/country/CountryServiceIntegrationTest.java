99b2600e02121373de46412589696c9cb32e3747
Caio <caio.vr12@gmail.com>
2025-11-19 06:54:57 -0300

    testa CountryService com verificações de integração

---
+++ src/test/java/caio/portfolio/livraria/service/country/CountryServiceIntegrationTest.java
@@ -0,0 +1,150 @@
+package caio.portfolio.livraria.service.country;
+
+import java.util.List;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.DirtiesContext;
+import org.springframework.test.context.jdbc.Sql;
+import org.springframework.transaction.annotation.Transactional;
+
+import caio.portfolio.livraria.exception.custom.CountryNotFoundException;
+import caio.portfolio.livraria.infrastructure.entity.country.dto.CreateCountryDTO;
+import caio.portfolio.livraria.infrastructure.entity.country.dto.ResponseCountryDTO;
+import caio.portfolio.livraria.infrastructure.repository.CountryRepository;
+import caio.portfolio.livraria.service.country.dto.CountryResultImplDTO;
+
+@DirtiesContext(classMode=DirtiesContext.ClassMode.BEFORE_EACH_TEST_METHOD)
+@SpringBootTest
+@AutoConfigureTestDatabase(replace=AutoConfigureTestDatabase.Replace.ANY)
+class CountryServiceIntegrationTest {
+
+	@Autowired private CountryService service;
+	@Autowired private CountryRepository repo;
+	
+	private static final String BRAZIL_VALID_CODE = "BR";
+	private static final String RAW_BRAZIL_CODE = "br ";
+	private static final String UNITED_STATES_VALID_CODE = "US";
+	private static final String BRAZIL_NAME = "Brazil";
+	private static final String NON_EXISTENT_COUNTRY_CODE = "ir";
+	private static final String INVALID_COUNTRY_CODE = "ur";
+	
+	private static final CreateCountryDTO CREATE_BRAZIL_DTO = CreateCountryDTO.builder()
+	    .isoAlpha2Code(RAW_BRAZIL_CODE)
+	    .build();
+	
+	@Test
+	@Transactional(readOnly=true)
+	@Sql("/sql/insert_country_list.sql")
+	@DisplayName("Deve retornar país ao buscar com 'id' existente")
+	void getCountryById_returnsCountry() {
+		ResponseCountryDTO brazilRespDTO = service.getCountryById(1);
+		Assertions.assertNotNull(brazilRespDTO);
+		Assertions.assertEquals(BRAZIL_VALID_CODE, brazilRespDTO.getIsoAlpha2Code());
+	}
+	
+	@Test
+	@Transactional(readOnly=true)
+	@Sql("/sql/insert_country_list.sql")
+	@DisplayName("Deve retornar 'CountryNotFoundException' ao buscar com 'id' inexistente")
+	void getCountryById_returnsException() {
+		Assertions.assertThrows(
+			CountryNotFoundException.class, 
+			() -> service.getCountryById(1000)
+		);
+	}
+	
+	@Test
+	@Transactional(readOnly=true)
+	@Sql("/sql/insert_country_list.sql")
+	@DisplayName("Deve retornar país ao buscar com 'isoAlpha2Code' existente")
+	void getCountryByIsoAlpha2Code_returnsCountry() {
+		ResponseCountryDTO result = service.getCountryByIsoAlpha2Code(RAW_BRAZIL_CODE);
+		Assertions.assertEquals(BRAZIL_VALID_CODE, result.getIsoAlpha2Code());
+		Assertions.assertEquals(BRAZIL_NAME, result.getName());
+	}
+	 
+	@Test
+	@Transactional(readOnly=true)
+	@Sql("/sql/insert_country_list.sql")
+	@DisplayName("Deve retornar 'CountryNotFoundException' ao buscar com 'isoAlpha2Code' não existente")
+	void getCountryByIsoAlpha2Code_returnsException() {
+	    Assertions.assertThrows(
+           CountryNotFoundException.class,
+           () -> service.getCountryByIsoAlpha2Code(NON_EXISTENT_COUNTRY_CODE)
+	    );
+	}
+	
+	@Test
+	@Transactional(readOnly=true)
+	@Sql("/sql/insert_country_list.sql")
+	@DisplayName("Deve retornar 'IllegalArgumentException' ao buscar com 'isoAlpha2Code' inválido")
+	void getCountryByIsoAlpha2Code_returnsIllegalArgExcep() {
+	    Assertions.assertThrows(
+           IllegalArgumentException.class,
+           () -> service.getCountryByIsoAlpha2Code(INVALID_COUNTRY_CODE)
+	    );
+	}
+	
+	@Test
+	@Transactional(readOnly=true)
+	@Sql("/sql/insert_country_list.sql")
+	@DisplayName("Deve retornar lista de dto's de países quando existem países cadastrados")
+	void getAllCountries_returnsResponseCountryDTOList() {
+		List<ResponseCountryDTO> result = service.getAllCountries();
+		Assertions.assertNotNull(result);
+		Assertions.assertEquals(10, result.size());
+		Assertions.assertEquals(BRAZIL_VALID_CODE, result.get(0).getIsoAlpha2Code());
+		Assertions.assertEquals(UNITED_STATES_VALID_CODE, result.get(1).getIsoAlpha2Code());
+	}
+	 
+	@Test
+	@Transactional(readOnly=true)
+	@DisplayName("Deve retornar lista vazia quando não existem países cadastrados")
+	void getAllCountries_returnsEmptyList() {
+		List<ResponseCountryDTO> result = service.getAllCountries();
+		Assertions.assertNotNull(result);
+		Assertions.assertTrue(result.isEmpty());
+		Assertions.assertEquals(0, result.size());
+	}
+	
+	@Test
+	@Transactional
+	@Sql("/sql/insert_country_list.sql")
+	@DisplayName("Deve retornar país existente quando 'isoAlpha2Code' já está cadastrado")
+	void createOrFindCountry_returnsExistingCountry() {
+		CountryResultImplDTO result = service.createOrFindCountry(CREATE_BRAZIL_DTO);
+	    Assertions.assertNotNull(result);
+	    Assertions.assertEquals(BRAZIL_VALID_CODE, result.getCountry().getIsoAlpha2Code());
+	    Assertions.assertEquals(BRAZIL_NAME, result.getCountry().getName());
+	    Assertions.assertFalse(result.isCreated());
+	}
+	 
+	@Test
+	@Transactional
+	@DisplayName("Deve criar novo país quando 'isoAlpha2Code' não existe")
+	void createOrFindCountry_createsNewCountry() {
+		long initialCount = repo.count();
+		CountryResultImplDTO result = service.createOrFindCountry(CREATE_BRAZIL_DTO);
+	    Assertions.assertNotNull(result);
+	    Assertions.assertEquals(BRAZIL_VALID_CODE, result.getCountry().getIsoAlpha2Code());
+	    Assertions.assertEquals(BRAZIL_NAME, result.getCountry().getName());
+	    Assertions.assertTrue(result.isCreated());
+	    Assertions.assertTrue(result.isCreated());
+	    Assertions.assertEquals(initialCount + 1, repo.count());
+	}
+	 
+	@Test
+	@Transactional
+	@Sql("/sql/insert_country_list.sql")
+	@DisplayName("Não deve salvar quando país já existe")
+	void createOrFindCountry_doesNotSaveWhenCountryExists() {
+		long initialCount = repo.count();
+		service.createOrFindCountry(CREATE_BRAZIL_DTO);
+		Assertions.assertEquals(initialCount, repo.count());
+	}
+}

dcf4744008c023c387ff0366eade15d85afd4b61
Caio <caio.vr12@gmail.com>
2025-11-26 16:06:28 -0300

    implementa método createPublisher para criação de nova editora

--- src/main/java/caio/portfolio/livraria/service/publisher/PublisherService.java
+++ src/main/java/caio/portfolio/livraria/service/publisher/PublisherService.java
@@ -1,10 +1,18 @@
 package caio.portfolio.livraria.service.publisher;
 
+import java.util.Optional;
+
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.stereotype.Service;
 
+import caio.portfolio.livraria.exception.custom.publisher.ConcurrentPublisherException;
+import caio.portfolio.livraria.exception.custom.publisher.PublisherAlreadyExistsException;
+import caio.portfolio.livraria.infrastructure.entity.publisher.Publisher;
 import caio.portfolio.livraria.infrastructure.entity.publisher.dto.CreatePublisherDTO;
 import caio.portfolio.livraria.infrastructure.entity.publisher.dto.ResponsePublisherDTO;
 import caio.portfolio.livraria.infrastructure.repository.PublisherRepository;
+import caio.portfolio.livraria.service.country.CountryService;
+import caio.portfolio.livraria.service.publisher.model.ResponsePublisherDTOCreator;
 import lombok.RequiredArgsConstructor;
 
 @Service
@@ -12,9 +20,29 @@ import lombok.RequiredArgsConstructor;
 public class PublisherService {
 
 	private final PublisherRepository repo;
+	private final CountryService countryService;
+	private final ResponsePublisherDTOCreator responsePublisherDTOCreator;
+	
+	private Publisher saveAndHandlePublisherConcurrency(Publisher publisher) {
+		try {
+			return repo.saveAndFlush(publisher);
+		}
+		catch(DataIntegrityViolationException e) {
+			Optional<Publisher> publisherOptional = repo.findByFullAddress(publisher.getFullAddress());
+			if(publisherOptional.isPresent()) return publisherOptional.get();
+			throw new ConcurrentPublisherException("Falha ao tentar criar nova editora com 'name': "+publisher.getName()+"; 'fullAddress': "+publisher.getFullAddress()+"; 'countryId': "+publisher.getCountry());
+		}
+	}
 
 	public ResponsePublisherDTO createPublisher(CreatePublisherDTO dto) {
-		// TODO 
-		return null;
+		Optional<Publisher> existingPublisher = repo.findByFullAddress(dto.getFullAddress());
+		if(existingPublisher.isPresent()) throw new PublisherAlreadyExistsException("Não foi possível criar nova editora. Editora com 'fullAddress': "+dto.getFullAddress()+" já existe");
+		Publisher newPublisher = Publisher.builder()
+			.name(dto.getName())
+			.fullAddress(dto.getFullAddress())
+			.country(countryService.getCountryById(dto.getCountryId()))
+			.build();
+		newPublisher = saveAndHandlePublisherConcurrency(newPublisher);
+		return responsePublisherDTOCreator.toResponsePublisherDTO(newPublisher);
 	}
 }

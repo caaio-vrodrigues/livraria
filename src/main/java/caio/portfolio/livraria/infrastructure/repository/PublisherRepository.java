a8b709b5c9fa98083ef1ae3843d5b50b9a97b5d6
Caio <caio.vr12@gmail.com>
2025-11-26 14:34:38 -0300

    adiciona findByFullAddress ao repositório da entidade Publisher

--- src/main/java/caio/portfolio/livraria/infrastructure/repository/PublisherRepository.java
+++ src/main/java/caio/portfolio/livraria/infrastructure/repository/PublisherRepository.java
@@ -1,9 +1,13 @@
 package caio.portfolio.livraria.infrastructure.repository;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import caio.portfolio.livraria.infrastructure.entity.publisher.Publisher;
 
 @Repository
-public interface PublisherRepository extends JpaRepository<Publisher, Long> {}
+public interface PublisherRepository extends JpaRepository<Publisher, Long> {
+	Optional<Publisher> findByFullAddress(String fullAddress);
+}

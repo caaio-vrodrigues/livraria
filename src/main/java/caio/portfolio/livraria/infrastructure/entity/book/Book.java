3dfbdedd80a6cd1f75341ecf25abf42c707991cf
Caio <caio.vr12@gmail.com>
2025-11-29 13:36:31 -0300

    cria classe abstrata Book para base das entidades concretas que representarão os livros

---
+++ src/main/java/caio/portfolio/livraria/infrastructure/entity/book/Book.java
@@ -0,0 +1,37 @@
+package caio.portfolio.livraria.infrastructure.entity.book;
+
+import caio.portfolio.livraria.infrastructure.entity.author.Author;
+import caio.portfolio.livraria.infrastructure.entity.publisher.Publisher;
+import jakarta.persistence.Column;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.MappedSuperclass;
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.experimental.SuperBuilder;
+
+@MappedSuperclass
+@EqualsAndHashCode(of="isbn")
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@SuperBuilder
+public abstract class Book {
+
+	@Column(name="title", nullable=false)
+	private String title;
+	
+	@ManyToOne(fetch=FetchType.LAZY)
+	@JoinColumn(name="author_id", nullable=false)
+	private Author author;
+	
+	@ManyToOne(fetch=FetchType.LAZY)
+	@JoinColumn(name="publisher_id", nullable=false)
+	private Publisher publisher;
+	
+	@Column(name="isbn", nullable=false, unique=true)
+	private String isbn;
+}

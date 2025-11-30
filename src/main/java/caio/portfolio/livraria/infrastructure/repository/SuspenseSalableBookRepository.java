9126b91ce5640eab89e69c8f0b37c1729496683b
Caio <caio.vr12@gmail.com>
2025-11-30 10:04:30 -0300

    cria entidade e repositório para o tema suspense

---
+++ src/main/java/caio/portfolio/livraria/infrastructure/repository/SuspenseSalableBookRepository.java
@@ -0,0 +1,9 @@
+package caio.portfolio.livraria.infrastructure.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import caio.portfolio.livraria.infrastructure.entity.book.salable.suspense.SuspenseSalableBook;
+
+@Repository
+public interface SuspenseSalableBookRepository extends JpaRepository<SuspenseSalableBook, Long> {}

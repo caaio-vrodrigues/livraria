6a3fce184fbb459306912a8080f6a1b6c9ec3308
Caio <caio.vr12@gmail.com>
2025-11-30 10:16:10 -0300

    cria entidade e repositório para o tema infantil

--- src/main/java/caio/portfolio/livraria/infrastructure/repository/book/SuspenseSalableBookRepository.java
+++ src/main/java/caio/portfolio/livraria/infrastructure/repository/book/SuspenseSalableBookRepository.java
@@ -1,4 +1,4 @@
-package caio.portfolio.livraria.infrastructure.repository;
+package caio.portfolio.livraria.infrastructure.repository.book;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;

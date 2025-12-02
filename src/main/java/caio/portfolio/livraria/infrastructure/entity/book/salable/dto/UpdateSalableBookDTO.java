d633d751ebf013927ac0f4920d8b33880a2934c9
Caio <caio.vr12@gmail.com>
2025-12-02 11:41:09 -0300

    cria DTO's de update para entidade SalableBook

---
+++ src/main/java/caio/portfolio/livraria/infrastructure/entity/book/salable/dto/UpdateSalableBookDTO.java
@@ -0,0 +1,15 @@
+package caio.portfolio.livraria.infrastructure.entity.book.salable.dto;
+
+import java.math.BigDecimal;
+
+import caio.portfolio.livraria.infrastructure.entity.book.dto.UpdateBookDTO;
+import lombok.Getter;
+import lombok.experimental.SuperBuilder;
+
+@Getter
+@SuperBuilder
+public class UpdateSalableBookDTO extends UpdateBookDTO {
+
+	private final BigDecimal price;
+	private final Integer units;
+}

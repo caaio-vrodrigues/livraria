49aefe8fc5eed93fe9ac2569211c7d8d5be5bb95
Caio <caio.vr12@gmail.com>
2025-12-10 12:37:29 -0300

    refatora testes unitários da classe ResponseAuthorDTOCreatorImpl

--- src/test/java/caio/portfolio/livraria/service/author/ResponseAuthorDTOCreatorImplTest.java
+++ src/test/java/caio/portfolio/livraria/service/author/ResponseAuthorDTOCreatorImplTest.java
@@ -2,7 +2,8 @@ package caio.portfolio.livraria.service.author;
 
 import java.time.LocalDate;
 
-import org.junit.jupiter.api.Assertions;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -18,28 +19,36 @@ class ResponseAuthorDTOCreatorImplTest {
 	
 	@InjectMocks private ResponseAuthorDTOCreatorImpl responseAuthorDTOCreatorImpl;
 	
-	private static final String EXPECTED_NAME = "Caio Vinicius Rodrigues";
-	private static final String EXPECTED_ALIAS = "Caio VR";
-	private static final LocalDate EXPECTED_BIRTHDAY = LocalDate.of(1992, 03, 20);
+	private static final String BRAZIL_CODE = "BR";
+	private static final String CAIO_ALIAS = "CVR";
+	private static final String CAIO_FULL_NAME = "Caio Vinicius Rodrigues";
+	private static final LocalDate CAIO_BIRTHDAY = LocalDate.of(1992, 03, 20);
 	
 	private static final Country BRAZIL = Country.builder()
-		.isoAlpha2Code("br")	
+		.isoAlpha2Code(BRAZIL_CODE)	
 		.build();
 	
 	private static final Author CAIO_VR = Author.builder()
-		.alias("Caio VR")
-		.fullName("Caio Vinicius Rodrigues")
-		.birthday(LocalDate.of(1992, 03, 20))
+		.alias(CAIO_ALIAS)
+		.fullName(CAIO_FULL_NAME)
+		.birthday(CAIO_BIRTHDAY)
 		.country(BRAZIL)
 		.build();
 	
 	@Test
 	@DisplayName("Deve converter 'Author' para 'ResponseAuthorDTO' e retorna-lo")
 	void toResponseAuthorDTO_returnsResponseAuthorDTO() {
-		ResponseAuthorDTO responseAuthorDTO = responseAuthorDTOCreatorImpl.toResponseAuthorDTO(CAIO_VR);
-		Assertions.assertNotNull(responseAuthorDTO);
-		Assertions.assertEquals(EXPECTED_NAME, responseAuthorDTO.getFullName());
-		Assertions.assertEquals(EXPECTED_ALIAS, responseAuthorDTO.getAlias());
-		Assertions.assertEquals(EXPECTED_BIRTHDAY, responseAuthorDTO.getBirthday());
+		ResponseAuthorDTO responseAuthorDTO = responseAuthorDTOCreatorImpl
+			.toResponseAuthorDTO(CAIO_VR);
+		assertNotNull(responseAuthorDTO);
+		assertEquals(
+			CAIO_FULL_NAME, 
+			responseAuthorDTO.getFullName());
+		assertEquals(
+			CAIO_ALIAS, 
+			responseAuthorDTO.getAlias());
+		assertEquals(
+			CAIO_BIRTHDAY, 
+			responseAuthorDTO.getBirthday());
 	}
 }

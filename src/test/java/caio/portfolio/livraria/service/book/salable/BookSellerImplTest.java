169fa4ed01dc5c610d150c2b04a50e2121b20d31
Caio <caio.vr12@gmail.com>
2025-12-14 16:52:12 -0300

    testa classe BookSellerImpl com testes unitários

---
+++ src/test/java/caio/portfolio/livraria/service/book/salable/BookSellerImplTest.java
@@ -0,0 +1,90 @@
+package caio.portfolio.livraria.service.book.salable;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+import java.util.Optional;
+
+import static org.mockito.Mockito.when;
+import static org.mockito.Mockito.anyLong;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import caio.portfolio.livraria.infrastructure.entity.author.Author;
+import caio.portfolio.livraria.infrastructure.entity.book.salable.SalableBook;
+import caio.portfolio.livraria.infrastructure.entity.country.Country;
+import caio.portfolio.livraria.infrastructure.entity.publisher.Publisher;
+import caio.portfolio.livraria.infrastructure.repository.SalableBookRepository;
+import caio.portfolio.livraria.model.enums.Genre;
+
+@ExtendWith(MockitoExtension.class)
+class BookSellerImplTest {
+
+	@InjectMocks private BookSellerImpl bookSellerImpl;
+	@Mock private SalableBookRepository repo;
+	
+	private static final int UNITS = 50;
+	private static final Long O_ALQUIMISTA_ID = 1L;
+	private static final Long PAULO_COELHO_ID = 1L;
+	private static final Long ROCCO_ID = 1L;
+	private static final String BRAZIL_NAME = "Brazil";
+	private static final String BRAZIL_CODE = "BR";
+	private static final String PAULO_COLEHO_ALIAS = "O Mago";
+	private static final String PAULO_COELHO_FULL_NAME = "Paulo Coelho";
+	private static final String ROCCO_NAME = "Rocco";
+	private static final String ROCCO_FULL_ADDRESS = "Rua do Passeio, 38, 11º andar, no Passeio Corporate";
+	private static final String O_ALQUIMISTA_ISBN = "abc123";
+	private static final String O_ALQUIMISTA_TITLE = "Current Title";
+	private static final Integer BRAZIL_ID = 1;
+	private static final LocalDate PAULO_COELHO_BIRTHDAY = LocalDate.of(1947, 8, 24);
+	private static final BigDecimal O_ALQUIMISTA_PRICE = BigDecimal.valueOf(39.5);
+	
+	private static final Country BRAZIL = Country.builder()
+		.id(BRAZIL_ID)
+		.isoAlpha2Code(BRAZIL_CODE)
+		.name(BRAZIL_NAME)
+		.build(); 
+	
+	private static final Author PAULO_COELHO = Author.builder()
+		.id(PAULO_COELHO_ID)
+		.alias(PAULO_COLEHO_ALIAS)
+		.fullName(PAULO_COELHO_FULL_NAME)
+		.birthday(PAULO_COELHO_BIRTHDAY)
+		.country(BRAZIL)
+		.build();
+	
+	private static final Publisher ROCCO_PUBLISHER = Publisher.builder()
+		.id(ROCCO_ID)
+		.name(ROCCO_NAME)
+		.country(BRAZIL)
+		.fullAddress(ROCCO_FULL_ADDRESS)
+		.build();
+	
+	private static final SalableBook O_ALQUIMISTA = SalableBook.builder()
+		.id(O_ALQUIMISTA_ID)
+		.author(PAULO_COELHO)	
+		.title(O_ALQUIMISTA_TITLE)
+		.genre(Genre.FANTASY)
+		.isbn(O_ALQUIMISTA_ISBN)
+		.publisher(ROCCO_PUBLISHER)
+		.price(O_ALQUIMISTA_PRICE)
+		.units(UNITS)
+		.build();
+	
+	@Test
+	@DisplayName("Deve realizar venda de 'SalableBook' com sucesso e retornar cálculo do total a pagar")
+	void sellBook_returnsBigDecimal() {
+		when(repo.findById(anyLong()))
+			.thenReturn(Optional.of(O_ALQUIMISTA));
+		BigDecimal totalToPay = bookSellerImpl.sellBook(O_ALQUIMISTA.getId(), 2);
+		assertNotNull(totalToPay);
+		assertEquals(
+			O_ALQUIMISTA.getPrice().multiply(BigDecimal.valueOf(2)), 
+			totalToPay);
+	}
+}

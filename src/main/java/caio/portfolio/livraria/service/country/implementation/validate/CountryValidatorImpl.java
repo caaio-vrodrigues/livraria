3c6551e0a67ff54367db60a4fce6b28891518451
Caio <caio.vr12@gmail.com>
2026-01-04 15:12:26 -0300

    refatora testes unitários da classe CountryValidatorImpl

--- src/main/java/caio/portfolio/livraria/service/country/implementation/validate/CountryValidatorImpl.java
+++ src/main/java/caio/portfolio/livraria/service/country/implementation/validate/CountryValidatorImpl.java
@@ -18,11 +18,13 @@ public class CountryValidatorImpl implements CountryValidator {
 
 	@Override
 	public String processIsoAlpha2Code(String isoAlpha2Code) {
-		boolean isIsoAlpha2CodeNullOrBlank = isoAlpha2Code == null || isoAlpha2Code.isBlank();
+		boolean isIsoAlpha2CodeNullOrBlank = isoAlpha2Code == null || 
+			isoAlpha2Code.isBlank();
 		if(isIsoAlpha2CodeNullOrBlank) throw countryExceptionCreator
 			.createIllegalArgumentExceptionByBlank();
 		String normalizedIsoAlpha2Code = isoAlpha2Code.trim().toUpperCase();
-		boolean validIsoAlpha2Code = validIsoCodes.contains(normalizedIsoAlpha2Code);
+		boolean validIsoAlpha2Code = validIsoCodes
+			.contains(normalizedIsoAlpha2Code);
 		if(!validIsoAlpha2Code) throw countryExceptionCreator
 			.createIllegalArgumentExceptionByInvalid(normalizedIsoAlpha2Code);
 		return normalizedIsoAlpha2Code;
@@ -30,11 +32,14 @@ public class CountryValidatorImpl implements CountryValidator {
 
 	@Override
 	public String resolveNameByIsoAlpha2Code(String isoAlpha2Code) {
-		return getNameByValidatedAndNormalizedIsoAlpha2Code(processIsoAlpha2Code(isoAlpha2Code));
+		return getNameByValidatedAndNormalizedIsoAlpha2Code(
+			processIsoAlpha2Code(isoAlpha2Code));
 	}
 
 	@Override
-	public String getNameByValidatedAndNormalizedIsoAlpha2Code(String validIsoAlpha2Code) {
+	public String getNameByValidatedAndNormalizedIsoAlpha2Code(
+		String validIsoAlpha2Code
+	){
 		return new Locale.Builder()
 			.setRegion(validIsoAlpha2Code)
 			.build()
--- src/test/java/caio/portfolio/livraria/service/country/implementation/validate/CountryValidatorImplTest.java
+++ src/test/java/caio/portfolio/livraria/service/country/implementation/validate/CountryValidatorImplTest.java
@@ -5,7 +5,6 @@ import java.util.Set;
 
 import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
@@ -13,9 +12,12 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
+import static org.mockito.Mockito.when;
+import static org.mockito.Mockito.anyString;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
 import org.mockito.junit.jupiter.MockitoExtension;
 
-import caio.portfolio.livraria.service.country.implementation.validate.CountryValidatorImpl;
 import caio.portfolio.livraria.service.country.model.create.CountryExceptionCreator;
 
 @ExtendWith(MockitoExtension.class)
@@ -32,9 +34,12 @@ class CountryValidatorImplTest {
 	private static final String VALID_FRANCE_CODE = "FR";
 	private static final String RAW_FRANCE_CODE = "fr ";
 	private static final String INVALID_COUNTRY_CODE = "UR";
-	private static final String INVALID_CODE = " ";
+	private static final String BLANK_CODE = " ";
+	
 	
 	private static final Set<String> CODES_LIST = new HashSet<>();
+	
+	private static final IllegalArgumentException EXCEPTION = new IllegalArgumentException();
 	
 	@BeforeEach
 	void setUp() {
@@ -59,27 +64,40 @@ class CountryValidatorImplTest {
 	@Test
 	@DisplayName("Deve lançar 'IllegalArgumentException' ao enviar argumento nulo")
 	void processIsoAlpha2Code_throwsExceptionForNull() {
+		when(countryExceptionCreator
+				.createIllegalArgumentExceptionByBlank())
+			.thenReturn(EXCEPTION);
         assertThrows(
         	IllegalArgumentException.class, 
         	() -> validator.processIsoAlpha2Code(null));
+        verify(countryExceptionCreator, times(1))
+        	.createIllegalArgumentExceptionByBlank();
     }
 	
 	@Test
 	@DisplayName("Deve lançar 'IllegalArgumentException' ao enviar argumento vazio")
     void processIsoAlpha2Code_throwsExceptionForBlank() {
+		when(countryExceptionCreator
+				.createIllegalArgumentExceptionByBlank())
+			.thenReturn(EXCEPTION);
 		assertThrows(
 			IllegalArgumentException.class, 
-			() -> validator.processIsoAlpha2Code(INVALID_CODE));
+			() -> validator.processIsoAlpha2Code(BLANK_CODE));
+		verify(countryExceptionCreator, times(1))
+    		.createIllegalArgumentExceptionByBlank();
     }
 	
 	@Test
 	@DisplayName("Deve lançar 'IllegalArgumentException' ao enviar argumento inválido")
     void processIsoAlpha2Code_throwsExceptionForInvalidCode() {
-        IllegalArgumentException exception = assertThrows(
-            IllegalArgumentException.class, 
-            () -> validator.processIsoAlpha2Code(INVALID_COUNTRY_CODE)
-        );
-        assertTrue(exception.getMessage().contains(INVALID_COUNTRY_CODE));
+		when(countryExceptionCreator
+				.createIllegalArgumentExceptionByInvalid(anyString()))
+			.thenReturn(EXCEPTION);
+        assertThrows(
+			IllegalArgumentException.class, 
+			() -> validator.processIsoAlpha2Code(INVALID_COUNTRY_CODE));
+        verify(countryExceptionCreator, times(1))
+    		.createIllegalArgumentExceptionByInvalid(anyString());
     }
 	
 	@Test
@@ -91,11 +109,16 @@ class CountryValidatorImplTest {
 	}
 	
 	@Test
-	@DisplayName("Deve propagar exceção ao receber código inválido")
+	@DisplayName("Deve propagar IllegalArgumentException ao receber código inválido")
 	void resolveNameByIsoAlpha2Code_throwsExceptionForInvalidCode() {
+		when(countryExceptionCreator
+				.createIllegalArgumentExceptionByInvalid(anyString()))
+			.thenReturn(EXCEPTION);
 		assertThrows(
 			IllegalArgumentException.class,
 			() -> validator.resolveNameByIsoAlpha2Code(INVALID_COUNTRY_CODE));
+		verify(countryExceptionCreator, times(1))
+			.createIllegalArgumentExceptionByInvalid(anyString());
 	}
 	
 	@Test

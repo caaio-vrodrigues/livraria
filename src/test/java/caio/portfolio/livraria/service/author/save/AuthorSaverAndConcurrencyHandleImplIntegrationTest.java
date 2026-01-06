08821af8a6f53d2360b6674ff2adc0e6ba766495
Caio <caio.vr12@gmail.com>
2026-01-06 14:03:41 -0300

    testa classe AuthorSaverAndConcurrencyHandleImpl com verificações de integração

---
+++ src/test/java/caio/portfolio/livraria/service/author/save/AuthorSaverAndConcurrencyHandleImplIntegrationTest.java
@@ -0,0 +1,60 @@
+package caio.portfolio.livraria.service.author.save;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+import java.time.LocalDate;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase.Replace;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.DirtiesContext;
+import org.springframework.test.annotation.DirtiesContext.ClassMode;
+import org.springframework.test.context.jdbc.Sql;
+
+import caio.portfolio.livraria.infrastructure.entity.author.Author;
+import caio.portfolio.livraria.infrastructure.entity.country.Country;
+import caio.portfolio.livraria.service.author.implementation.save.AuthorSaverAndConcurrencyHandleImpl;
+
+@SpringBootTest
+@AutoConfigureTestDatabase(replace=Replace.ANY)
+@DirtiesContext(classMode=ClassMode.BEFORE_EACH_TEST_METHOD)
+class AuthorSaverAndConcurrencyHandleImplIntegrationTest {
+
+	@Autowired private AuthorSaverAndConcurrencyHandleImpl authorSaverAndConcurrencyHandleImpl;
+	
+	private static final String BRAZIL_NAME = "Brazil";
+	private static final String BRAZIL_CODE = "BR";
+	private static final String PAULO_COLEHO_ALIAS = "O Mago";
+	private static final String PAULO_COELHO_FULL_NAME = "Paulo Coelho";
+	private static final Integer BRAZIL_ID = 1;
+	private static final LocalDate PAULO_COELHO_BIRTHDAY = LocalDate.of(1947, 8, 24);
+	
+	private static final Country BRAZIL = Country.builder()
+		.id(BRAZIL_ID)
+		.isoAlpha2Code(BRAZIL_CODE)
+		.name(BRAZIL_NAME)
+		.build(); 
+	
+	private static final Author PAULO_COELHO = Author.builder()
+		.alias(PAULO_COLEHO_ALIAS)
+		.fullName(PAULO_COELHO_FULL_NAME)
+		.birthday(PAULO_COELHO_BIRTHDAY)
+		.country(BRAZIL)
+		.build();
+	
+	@Test
+	@Sql("/sql/country/insert_country_list.sql")
+	@DisplayName("Deve salvar e retornar autor com sucesso")
+	void saveAndHandleConcurrentyAuthor_returnsAuthor() {
+		Author pauloCoelho = authorSaverAndConcurrencyHandleImpl
+			.saveAndHandleConcurrentyAuthor(PAULO_COELHO);
+		assertNotNull(pauloCoelho);
+		assertEquals(
+			PAULO_COLEHO_ALIAS, 
+			pauloCoelho.getAlias());
+	}
+}

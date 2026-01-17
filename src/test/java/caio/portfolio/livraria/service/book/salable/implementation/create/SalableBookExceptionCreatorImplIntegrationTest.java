93580b781e74bbba2d9e57d0259ef905d49b85f7
Caio <caio.vr12@gmail.com>
2026-01-17 07:39:30 -0300

    refatora testes de integração da classe SalableBookExceptionCreatorImpl

--- src/test/java/caio/portfolio/livraria/service/book/salable/implementation/create/SalableBookExceptionCreatorImplIntegrationTest.java
+++ src/test/java/caio/portfolio/livraria/service/book/salable/implementation/create/SalableBookExceptionCreatorImplIntegrationTest.java
@@ -9,6 +9,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase.Replace;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.MessageSource;
+import org.springframework.context.i18n.LocaleContextHolder;
 import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.annotation.DirtiesContext.ClassMode;
 
@@ -23,59 +25,64 @@ import caio.portfolio.livraria.exception.custom.book.salable.SalableBookNotFound
 class SalableBookExceptionCreatorImplIntegrationTest {
 
 	@Autowired private SalableBookExceptionCreatorImpl salableBookExceptionCreatorImpl;
+	@Autowired private MessageSource salableBookMessageSource;
 	
 	private static final int UNITS = 100;
 	private static final Long O_ALQUIMISTA_ID = 1L;
 	private static final String PAULO_COELHO_FULL_NAME = "Paulo Coelho";
 	private static final String O_ALQUIMISTA_TITLE = "O Alquimista";
-	private static final String SALABLE_BOOK_ALREADY_EXISTS_MSG = "Não foi possível realizar a operação. Livro com `title`: `"+O_ALQUIMISTA_TITLE+"` e `author`: `"+PAULO_COELHO_FULL_NAME+"` já existe";
-	private static final String CONCURRENT_BOOK_MSG = "Não foi possível criar livro: `"+O_ALQUIMISTA_TITLE+"` por falha de concorrência. Verifique se o livro já existe ou tente novamente se necessário";
-	private static final String BOOK_NOT_FOUND_ID_MSG = "Não foi possível encontrar livro com `id`: `"+O_ALQUIMISTA_ID+"`";
-	private static final String INSUFICIENT_BOOK_UNITS_MSG = "Quantidade de livros insuficiente para realizar a venda. Estoque atual: `"+UNITS+"`";
 	
 	@Test
 	@DisplayName("Deve criar e retornar 'SalableBookAlreadyExistsException'")
 	void createSalableBookAlreadyExistsException() {
+		String expectedMsg = salableBookMessageSource.getMessage(
+	        "book.already.exists.tiltleAndAuthor",
+	        new Object[]{O_ALQUIMISTA_TITLE, PAULO_COELHO_FULL_NAME},
+	        LocaleContextHolder.getLocale());
 		SalableBookAlreadyExistsException exception = salableBookExceptionCreatorImpl
 			.createSalableBookAlreadyExistsException(
 				PAULO_COELHO_FULL_NAME, 
 				O_ALQUIMISTA_TITLE);
 		assertNotNull(exception);
-		assertEquals(
-			SALABLE_BOOK_ALREADY_EXISTS_MSG,
-			exception.getLocalizedMessage());
+		assertEquals(expectedMsg, exception.getLocalizedMessage());
 	}
 	
 	@Test
 	@DisplayName("Deve criar e retornar 'ConcurrentSalableBookException'")
 	void createConcurrentSalableBookException() {
+		String expectedMsg = salableBookMessageSource.getMessage(
+	        "concurrent.book",
+	        new Object[]{O_ALQUIMISTA_TITLE},
+	        LocaleContextHolder.getLocale());
 		ConcurrentSalableBookException exception = salableBookExceptionCreatorImpl
 			.createConcurrentSalableBookException(O_ALQUIMISTA_TITLE);
 		assertNotNull(exception);
-		assertEquals(
-			CONCURRENT_BOOK_MSG,
-			exception.getLocalizedMessage());
+		assertEquals(expectedMsg, exception.getLocalizedMessage());
 	}
 	
 	@Test
 	@DisplayName("Deve criar e retornar 'SalableBookNotFoundException'")
 	void createSalableBookNotFoundException() {
+		String expectedMsg = salableBookMessageSource.getMessage(
+	        "book.not.found.id",
+	        new Object[]{O_ALQUIMISTA_ID},
+	        LocaleContextHolder.getLocale());
 		SalableBookNotFoundException exception = salableBookExceptionCreatorImpl
 			.createSalableBookNotFoundException(O_ALQUIMISTA_ID);
 		assertNotNull(exception);
-		assertEquals(
-			BOOK_NOT_FOUND_ID_MSG,
-			exception.getLocalizedMessage());
+		assertEquals(expectedMsg, exception.getLocalizedMessage());
 	}
 	
 	@Test
 	@DisplayName("Deve criar e retornar 'InsuficientSalableBookUnitsException'")
 	void createInsuficientSalableBookUnitsException() {
+		String expectedMsg = salableBookMessageSource.getMessage(
+	        "insuficient.book.units",
+	        new Object[]{UNITS},
+	        LocaleContextHolder.getLocale());
 		InsuficientSalableBookUnitsException exception = salableBookExceptionCreatorImpl
 			.createInsuficientSalableBookUnitsException(UNITS);
 		assertNotNull(exception);
-		assertEquals(
-			INSUFICIENT_BOOK_UNITS_MSG,
-			exception.getLocalizedMessage());
+		assertEquals(expectedMsg, exception.getLocalizedMessage());
 	}
 }

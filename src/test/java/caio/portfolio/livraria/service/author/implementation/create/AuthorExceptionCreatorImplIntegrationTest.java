1e60af9b5ceed133211c1604aaacf872f2d1d786
Caio <caio.vr12@gmail.com>
2026-01-17 07:44:35 -0300

    refatora testes de integração da classe AuthorExceptionCreatorImpl

--- src/test/java/caio/portfolio/livraria/service/author/implementation/create/AuthorExceptionCreatorImplIntegrationTest.java
+++ src/test/java/caio/portfolio/livraria/service/author/implementation/create/AuthorExceptionCreatorImplIntegrationTest.java
@@ -9,6 +9,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase.Replace;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.MessageSource;
+import org.springframework.context.i18n.LocaleContextHolder;
 import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.annotation.DirtiesContext.ClassMode;
 
@@ -21,34 +23,37 @@ import caio.portfolio.livraria.exception.custom.author.AuthorNotFoundException;
 class AuthorExceptionCreatorImplIntegrationTest {
 
 	@Autowired private AuthorExceptionCreatorImpl authorExceptionCreatorImpl;
+	@Autowired private MessageSource authorMessageSource;
 	
 	private static final Long MONTEIRO_LOBATO_ID = 6L;
 	private static final String MONTEIRO_LOBATO_NAME = "Monteiro Lobato";
 	private static final String MONTEIRO_LOBATO_ALIAS = "monteiro.lob";
-	private static final String AUTHOR_ALREADY_EXISTS_MSG = "`alias`: `"+MONTEIRO_LOBATO_ALIAS+"` já está sendo utilizado pelo autor: `"+MONTEIRO_LOBATO_NAME+"`";
-	private static final String AUTHOR_NOT_FOUND_MSG = "Não foi possível encontrar um autor com `id`: `"+MONTEIRO_LOBATO_ID+"`";
 	
 	@Test
 	@DisplayName("Deve retornar 'AuthorAlreadyExistsException'")
 	void createAuthorAlreadyExistsException() {
+		String expectedMsg = authorMessageSource.getMessage(
+			"author.already.exists.alias", 
+			new Object[]{MONTEIRO_LOBATO_ALIAS, MONTEIRO_LOBATO_NAME},
+			LocaleContextHolder.getLocale());
 		AuthorAlreadyExistsException exception = authorExceptionCreatorImpl
 			.createAuthorAlreadyExistsException(
 				MONTEIRO_LOBATO_ALIAS, 
 				MONTEIRO_LOBATO_NAME);
 		assertNotNull(exception);
-		assertEquals(
-			AUTHOR_ALREADY_EXISTS_MSG, 
-			exception.getLocalizedMessage());
+		assertEquals(expectedMsg, exception.getLocalizedMessage());
 	}
 	
 	@Test
 	@DisplayName("Deve retornar 'AuthorNotFoundException'")
 	void createAuthorNotFoundException() {
+		String expectedMsg = authorMessageSource.getMessage(
+			"author.not.found.id", 
+			new Object[]{MONTEIRO_LOBATO_ID},
+			LocaleContextHolder.getLocale());
 		AuthorNotFoundException exception = authorExceptionCreatorImpl
 			.createAuthorNotFoundException(MONTEIRO_LOBATO_ID);
 		assertNotNull(exception);
-		assertEquals(
-			AUTHOR_NOT_FOUND_MSG, 
-			exception.getLocalizedMessage());
+		assertEquals(expectedMsg, exception.getLocalizedMessage());
 	}
 }

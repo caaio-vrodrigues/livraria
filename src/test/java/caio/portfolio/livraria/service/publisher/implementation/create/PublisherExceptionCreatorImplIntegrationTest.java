ea0f3181d7c3ad53eedd73ce553b0b97b79002b1
Caio <caio.vr12@gmail.com>
2026-01-17 07:18:26 -0300

    refatora testes de integração da classe PublisherExceptionCreatorImpl

--- src/test/java/caio/portfolio/livraria/service/publisher/implementation/create/PublisherExceptionCreatorImplIntegrationTest.java
+++ src/test/java/caio/portfolio/livraria/service/publisher/implementation/create/PublisherExceptionCreatorImplIntegrationTest.java
@@ -1,6 +1,7 @@
 package caio.portfolio.livraria.service.publisher.implementation.create;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -8,6 +9,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase.Replace;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.MessageSource;
+import org.springframework.context.i18n.LocaleContextHolder;
 import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.annotation.DirtiesContext.ClassMode;
 
@@ -17,41 +20,51 @@ import org.springframework.test.annotation.DirtiesContext.ClassMode;
 class PublisherExceptionCreatorImplIntegrationTest {
 
 	@Autowired private PublisherExceptionCreatorImpl publisherExceptionCreatorImpl;
+	@Autowired private MessageSource publisherMessageSource;
 	
 	private static final Long ROCCO_ID = 1L;
 	private static final String ROCCO_NAME = "Rocco";
 	private static final String ROCCO_END = "Rua do Passeio, 38, 11º andar, no Passeio Corporate";
-	private static final String CONCURRENT_PUBLISHER_EXCEPTION_MESSAGE = "Falha ao tentar criar nova editora: `"+ROCCO_NAME+"`";
-	private static final String PUBLISHER_ALREADY_EXISTS_EXCEPTION_MESSAGE = "Não foi possível criar nova editora. Editora com `fullAddress`: `"+ROCCO_END+"` já existe";
-	private static final String PUBLISHER_NOT_FOUND_EXCEPTION_MESSAGE = "Não foi possível encontrar editora com `id`: `"+ROCCO_ID+"`";
 	
 	@Test
 	@DisplayName("Deve lançar 'ConcurrentPublisherException'")
 	void createConcurrentPublisherException() {
-		assertEquals(
-			CONCURRENT_PUBLISHER_EXCEPTION_MESSAGE, 
-			publisherExceptionCreatorImpl
-				.createConcurrentPublisherException(ROCCO_NAME)
-					.getLocalizedMessage());
+		String expectedMsg = publisherMessageSource.getMessage(
+			"publisher.concurrent", 
+			new Object[]{ROCCO_NAME},
+			LocaleContextHolder.getLocale());
+		String msg = publisherExceptionCreatorImpl
+			.createConcurrentPublisherException(ROCCO_NAME)
+			.getLocalizedMessage();
+		assertNotNull(msg);
+		assertEquals(expectedMsg, msg);
 	}
 	
 	@Test
 	@DisplayName("Deve lançar 'PublisherAlreadyExistsException'")
 	void createPublisherAlreadyExistsException() {
-		assertEquals(
-			PUBLISHER_ALREADY_EXISTS_EXCEPTION_MESSAGE, 
-			publisherExceptionCreatorImpl
-				.createPublisherAlreadyExistsException(ROCCO_END)
-					.getLocalizedMessage());
+		String expectedMsg = publisherMessageSource.getMessage(
+			"publisher.already.exists.fullAddress", 
+			new Object[]{ROCCO_END},
+			LocaleContextHolder.getLocale());
+		String msg = publisherExceptionCreatorImpl
+			.createPublisherAlreadyExistsException(ROCCO_END)
+			.getLocalizedMessage();
+		assertNotNull(msg);
+		assertEquals(expectedMsg, msg);
 	}
 	
 	@Test
 	@DisplayName("Deve lançar 'PublisherNotFoundException'")
 	void createPublisherNotFoundException() {
-		assertEquals(
-			PUBLISHER_NOT_FOUND_EXCEPTION_MESSAGE, 
-			publisherExceptionCreatorImpl
-				.createPublisherNotFoundException(ROCCO_ID)
-					.getLocalizedMessage());
+		String expectedMsg = publisherMessageSource.getMessage(
+			"publisher.not.found.id.throws", 
+			new Object[]{ROCCO_ID},
+			LocaleContextHolder.getLocale());
+		String msg = publisherExceptionCreatorImpl
+			.createPublisherNotFoundException(ROCCO_ID)
+			.getLocalizedMessage();
+		assertNotNull(msg);
+		assertEquals(expectedMsg, msg);
 	}
 }
